356d21e21b8fc9aa14adc9e6d6f95702c2312cff
TlacaelelGD <147206125+TlacaelelGD@users.noreply.github.com>
2023-11-25 11:22:38 -0600

    v

--- javascrip/scrips.js
+++ javascrip/scrips.js
@@ -24,4 +24,46 @@ function mostrarDescripcion(elemento) {
 
   setInterval(nextSlide, 3000); // Cambia de slide cada 3 segundos (ajusta según sea necesario)
 
-  
+  function mostrarVistaPrevia() {
+    const input = document.getElementById('imagen');
+    const vistaPrevia = document.getElementById('vista-previa');
+
+    // Verifica si se seleccionó un archivo
+    if (input.files && input.files[0]) {
+        const lector = new FileReader();
+
+        lector.onload = function (e) {
+            vistaPrevia.src = e.target.result;
+            vistaPrevia.style.display = 'block';
+        };
+
+        lector.readAsDataURL(input.files[0]);
+    }
+}
+
+// Escucha el cambio en el input de la imagen
+document.getElementById('imagen').addEventListener('change', mostrarVistaPrevia);
+/*function enviarFormulario() {
+    const formulario = document.getElementById('formulario');
+            const datos = new FormData(formulario);
+
+            // Enviar los datos al backend (cambia la URL según tu configuración)
+            fetch('URL_DEL_BACKEND', {
+                method: 'POST',
+                body: datos
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Aquí puedes manejar la respuesta del backend
+                console.log('Respuesta del backend:', data);
+
+                // Después de enviar el formulario, redirige a la página de productos
+                window.location.href = 'Productos.html';
+            })
+            .catch(error => {
+                console.error('Error al enviar el formulario:', error);
+            });
+        }*/
+
+    // Después de enviar el formulario, redirige a la página de productos
+    //window.location.href = 'Productos.html';
